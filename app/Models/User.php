5f8cef7eb0b9b7101bfc7d42504e0bb7b0faa553
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-10-25 22:15:01 +0100

    removing the profile model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,15 +11,12 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are not mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -40,8 +37,62 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function profile()
+
+    public function user()
     {
-        return $this->hasOne(Profile::class);
+        return $this->belongsTo(User::class);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function reactions()
+    {
+        return $this->hasMany(Reaction::class);
+    }
+
+    public function friends()
+    {
+        return $this->belongsToMany(Profile::class, 'friends', 'profile_id1', 'profile_id2')
+            ->withTimestamps();
+    }
+
+    // send a message to a specific user this logic should be on the controller side but it is here for the testing purpose .
+    public function send_message(Profile $profile, $content)
+    {
+        $this->messages_received()->attach($profile, ['content' => $content,]);
+    }
+
+    // @return the messages sent by the user .
+    public function messages_sent()
+    {
+        return $this->belongsToMany(Profile::class, 'messages', 'receiver', 'sender')
+            ->withTimestamps()
+            ->withPivot('content');
+    }
+
+    // @return the messages received to the user .
+    public function messages_received()
+    {
+        return $this->belongsToMany(Profile::class, 'messages', 'sender', 'receiver')
+            ->withTimestamps()->withPivot('content');
+    }
+
+    public function messages_received_from(Profile $profile)
+    {
+        return $this->messages_received->where('id', $profile->id);
+    }
+
+    public function messages_sent_to(Profile $profile)
+    {
+        return $this->messages_sent->where('id', $profile->id);
+    }
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('gender');
+            $table->date('date_birth');
+            $table->smallInteger('state');
             $table->rememberToken();
             $table->timestamps();
         });
