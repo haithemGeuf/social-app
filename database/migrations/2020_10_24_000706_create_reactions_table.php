359b28c4d38e239bee838455e9cc0d27c30307c8
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-10-26 05:09:56 +0100

    setting the relationships with the Models

--- database/migrations/2020_10_24_000706_create_reactions_table.php
+++ database/migrations/2020_10_24_000706_create_reactions_table.php
@@ -15,6 +15,8 @@ class CreateReactionsTable extends Migration
     {
         Schema::create('reactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('posts_id')->constrained();
             $table->timestamps();
         });
     }
